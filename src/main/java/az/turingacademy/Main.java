c71eb451264a6e3d352fcbc9f9d560d53ec7f2d5
LalaHamidova1210 <lalahamidova1210@gmail.com>
2024-10-31 02:24:44 +0400

    main java added

---
+++ src/main/java/az/turingacademy/Main.java
@@ -0,0 +1,31 @@
+package az.turingacademy;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int string1 = 20;
+        int string2 =++string1;
+        System.out.println ("string1="+string1+", string2="+string2);
+        int string3= 10;
+        int string4=--string3;
+        System.out.println ("string3="+string3+", string4="+string4);
+        int string5=40;
+        string5+=5;
+        System.out.println( "new string:" +string5);
+        int string6= 50;
+        string6-=10;
+        System.out.println ("new string:" +string6);
+        double number=30.0;
+        number /=4;
+        System.out.println ("result"+number);
+        int number10 =25;
+        number10 %=4;
+        System.out.println ("result"+number10);
+
+
+
+
+
+        }
+    }
